20944134d88fbff16b640e4a3d342a9ab7a5ad38
Brodie Ashcraft <brodiea19@gmail.com>
2023-06-22 11:27:19 -0700

    created card, started building card component

---
+++ src/components/Cards/TopCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TopCard({ top }) {
+  return <img className="content-image" src={top.image}></img>;
+}
+
+export default TopCard;
--- src/components/Content-and-Nav/Tops.jsx
+++ src/components/Content-and-Nav/Tops.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { HomeLink } from "./NavLinks";
 import { useEffect } from "react";
+import { useState } from "react";
+import TopCard from "../Cards/TopCard";
 
 function Tops() {
+  const [tops, setTops] = useState([]);
+
   useEffect(() => {
     fetch("http://localhost:4000/clothing/")
       .then((r) => r.json())
-      .then((data) => console.log(data.tops));
+      .then((data) => setTops(data.tops));
   }, []);
+
+  const topsToDisplay = tops.map((top) => {
+    return <TopCard top={top} />;
+  });
 
   return (
     <>
       <HomeLink />
       <h2>Tops</h2>
+      <div>{topsToDisplay}</div>
     </>
   );
 }
