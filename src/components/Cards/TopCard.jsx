17d74072597a07bfbea2be8406eff69bbe3aed1c
Brodie Ashcraft <brodiea19@gmail.com>
2023-06-24 13:53:51 -0700

    built card component out, created modal element

--- src/components/Cards/TopCard.jsx
+++ src/components/Cards/TopCard.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { useState } from "react";
+import BuyModal from "../Content-and-Nav/BuyModal";
 
 function TopCard({ top }) {
+  const { image, description, price, info, sizes } = top;
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>
-      <img className="content-image" src={top.image}></img>
+    <div className="top-card">
+      <img className="content-image" src={image}></img>
+      <h1>{description}</h1>
+      <h2>{info}</h2>
+      <h2>{price}</h2>
+      <select className="size-dropdown">
+        <option>Size</option>
+        <option>{sizes[0].size}</option>
+        <option>{sizes[1].size}</option>
+        <option>{sizes[2].size}</option>
+      </select>
+      <BuyModal />
     </div>
   );
 }
