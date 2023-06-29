45d9c58f0a5af25c0ac5cea5dcb0ca9528ca613e
Brodie Ashcraft <brodiea19@gmail.com>
2023-06-29 12:22:10 -0700

    added mapped out components to bottoms component

--- src/components/Content-and-Nav/Bottoms.jsx
+++ src/components/Content-and-Nav/Bottoms.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { HomeLink } from "./NavLinks";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import TopCard from "../Cards/TopCard";
 
 function Bottoms() {
+  const [bottoms, setBottoms] = useState([]);
+
   useEffect(() => {
     fetch("https://phase-2-project-backend.onrender.com/bottoms")
       .then((r) => r.json())
-      .then((data) => console.log(data));
+      .then((data) => setBottoms(data));
   }, []);
+
+  const bottomsToDisplay = bottoms.map((bottom) => {
+    return <TopCard key={bottom.id} top={bottom} typeOf="bottoms" />;
+  });
 
   return (
     <>
       <HomeLink />
       <h2>Bottoms</h2>
+      <div>{bottomsToDisplay}</div>
     </>
   );
 }
