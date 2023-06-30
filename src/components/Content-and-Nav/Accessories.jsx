86cfe084f85e06390f6d00e8df4a2268bb8d5a14
Brodie Ashcraft <brodiea19@gmail.com>
2023-06-30 09:41:58 -0700

    mapped out items in Accessories component

--- src/components/Content-and-Nav/Accessories.jsx
+++ src/components/Content-and-Nav/Accessories.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { HomeLink } from "./NavLinks";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import TopCard from "../Cards/TopCard";
+import AccessoryCard from "../Cards/AccessoryCard";
 
 function Accessories() {
+  const [accessories, setAccessories] = useState([]);
+
   useEffect(() => {
     fetch("https://phase-2-project-backend.onrender.com/accessories")
       .then((r) => r.json())
-      .then((data) => console.log(data));
+      .then((data) => setAccessories(data));
   }, []);
+
+  const displayAccessories = accessories.map((item) => {
+    return <AccessoryCard id={item.id} accessory={item} typeOf="accessories" />;
+  });
 
   return (
     <>
       <HomeLink />
       <h2>Accessories</h2>
+      <div className="accessories-container">{displayAccessories}</div>
     </>
   );
 }
