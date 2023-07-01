3e456db0353df7807aeac9d2eb932c9328b18d09
Brodie Ashcraft <brodiea19@gmail.com>
2023-06-30 22:03:39 -0700

    added picture bar to homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,30 @@
 import React from "react";
-
 import { NavLinks } from "../Content-and-Nav/NavLinks";
 import Header from "./Header";
 import NewItem from "../Content-and-Nav/NewItem";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async (endPoint) => {
+      const fetchedObj = await fetch(
+        `https://phase-2-project-backend.onrender.com/${endPoint}`
+      );
+      const jsonResponse = await fetchedObj.json();
+      const imageArr = jsonResponse.map((obj) => obj.image);
+      setImages((prevImage) => [...prevImage, imageArr]);
+    };
+    ["tops", "bottoms", "accessories"].forEach((ep) => fetchData(ep));
+  }, []);
+
+  const imagesDisplay = images.map((img) => {
+    return (
+      <img key={img} src={img} className="home-picture" alt="failure"></img>
+    );
+  });
+
   return (
     <>
       <Header />
@@ -12,6 +32,7 @@ function Home() {
         <NavLinks />
         <NewItem />
       </div>
+      <div className="picture-bar">{imagesDisplay}</div>
     </>
   );
 }
